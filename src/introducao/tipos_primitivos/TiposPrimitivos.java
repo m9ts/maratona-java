4378b576ec50bfa98df453af7ffde04fd6967c2a
m9ts <mat3us.gois@gmail.com>
2025-09-18 17:02:18 -0300

    Convenções de variáveis, declaração e tamanho em memória

---
+++ src/introducao/tipos_primitivos/TiposPrimitivos.java
@@ -0,0 +1,22 @@
+package introducao.tipos_primitivos;
+
+public class TiposPrimitivos {
+    public static void main(String[] args) {
+        // int, double, float, byte, short, long, boolean
+
+        int age = 10;
+        long numeroGrande = 100000;
+        double salarioDouble = 2000;
+        float salarioFloat = 2500;
+        byte idadeByte = -128;
+        short idadeShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 77; // ASCII table
+        char charUnicode = '\u0043';
+
+        System.out.println("A idade é " + age + " anos.");
+        System.out.println(idadeByte);
+        System.out.println(charUnicode);
+    }
+}
