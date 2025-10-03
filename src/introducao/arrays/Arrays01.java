03948dbfa60fd091316dda59dc517be75e6d3068
m9ts <mat3us.gois@gmail.com>
2025-10-03 15:08:26 -0300

    Introdução a Arrays

---
+++ src/introducao/arrays/Arrays01.java
@@ -0,0 +1,15 @@
+package introducao.arrays;
+
+public class Arrays01 {
+    public static void main(String[] args) {
+//        Arrays são considerados objetos na memória
+        int[] idades = new int[3];
+        idades[0] = 21;
+        idades[1] = 22;
+        idades[2] = 23;
+
+        System.out.println(idades[0]);
+        System.out.println(idades[1]);
+        System.out.println(idades[2]);
+    }
+}
