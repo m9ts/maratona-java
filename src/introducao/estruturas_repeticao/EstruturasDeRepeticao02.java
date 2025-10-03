14bcc0bdb7f0239e5703fd382c82cbc34ea145be
m9ts <mat3us.gois@gmail.com>
2025-10-03 14:31:17 -0300

    Break dentro do If

---
+++ src/introducao/estruturas_repeticao/EstruturasDeRepeticao02.java
@@ -0,0 +1,15 @@
+package introducao.estruturas_repeticao;
+
+public class EstruturasDeRepeticao02 {
+    //    Imprima os primeiros 25 números de um dado valor. Por ex.: 50
+    public static void main(String[] args) {
+        int valorMax = 50;
+        for (int i = 0; i <= valorMax; i++) {
+            if (i > 25) {
+                break;
+            }
+            System.out.println("i = " + i);
+        }
+
+    }
+}
