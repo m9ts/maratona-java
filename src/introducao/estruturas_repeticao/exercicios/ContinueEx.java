5a1ce1c9825b4b6e19ca2804be9014345de169b4
m9ts <mat3us.gois@gmail.com>
2025-10-03 14:55:58 -0300

    Exercício utilizando continue

---
+++ src/introducao/estruturas_repeticao/exercicios/ContinueEx.java
@@ -0,0 +1,17 @@
+package introducao.estruturas_repeticao.exercicios;
+
+public class ContinueEx {
+    //    Dado o valor de um carro, descubra em quantas vezes ele pode ser parcelado
+//    Condição: valorParcela >= 1000
+    public static void main(String[] args) {
+        double valorTotal = 30000;
+        for (int parcela = (int) valorTotal; parcela >= 1; parcela--) {
+
+            double valorParcela = valorTotal / parcela;
+            if (valorParcela < 1000) {
+                continue;
+            }
+            System.out.println("Parcela: " + parcela + " | R$ " + valorParcela);
+        }
+    }
+}
