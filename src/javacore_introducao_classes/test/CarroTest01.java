28ed15de84123f7e3c9705746dc7c889c7df45d1
m9ts <mat3us.gois@gmail.com>
2025-10-10 12:21:52 -0300

    Referência de objetos

--- src/javacore_introducao_classes/test/CarroTest01.java
+++ src/javacore_introducao_classes/test/CarroTest01.java
@@ -4,18 +4,24 @@ import javacore_introducao_classes.dominio.Carro;
 
 public class CarroTest01 {
     public static void main(String[] args) {
-        Carro carroEsportivo = new Carro();
-        carroEsportivo.nome = "M4 Competition";
-        carroEsportivo.ano = 2025;
-        carroEsportivo.modelo = "Coupé";
-        System.out.println("----- CARRO ESPORTIVO ----");
-        System.out.println("Nome: " + carroEsportivo.nome + "\nModelo: " + carroEsportivo.modelo + "\nAno: " + carroEsportivo.ano);
+        Carro carro1 = new Carro();
+        carro1.nome = "M4 Competition";
+        carro1.ano = 2025;
+        carro1.modelo = "Coupé";
 
-        Carro carroAntigo = new Carro();
-        carroAntigo.nome = "Opala";
-        carroAntigo.ano = 1976;
-        carroAntigo.modelo = "Comodoro";
+        Carro carro2 = new Carro();
+        carro2.nome = "Opala";
+        carro2.ano = 1976;
+        carro2.modelo = "Comodoro";
+
+        // Referência de ojbetos
+        carro2 = carro1; // isso só pode ser feito com objetos do mesmo tipo
+
+        System.out.println("----- CARRO ESPORTIVO ----");
+        System.out.println("Nome: " + carro1.nome + "\nModelo: " + carro1.modelo + "\nAno: " + carro1.ano);
+
         System.out.println("\n----- CARRO ANTIGO ----");
-        System.out.println("Nome: " + carroAntigo.nome + "\nModelo: " + carroAntigo.modelo + "\nAno: " + carroAntigo.ano);
+        System.out.println("Nome: " + carro2.nome + "\nModelo: " + carro2.modelo + "\nAno: " + carro2.ano);
+
     }
 }
