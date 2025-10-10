6e0328bbf5d47a31837a756b5d35cc820d2f5308
m9ts <mat3us.gois@gmail.com>
2025-10-10 12:47:23 -0300

    Utilização de métodos

---
+++ src/javacore/metodos/dominio/Calculadora.java
@@ -0,0 +1,12 @@
+package javacore.metodos.dominio;
+
+public class Calculadora {
+
+    public void soma() {
+        System.out.println(10 + 10);
+    }
+
+    public void subtracao() {
+        System.out.println(21 - 10);
+    }
+}
---
+++ src/javacore/metodos/test/CalculadoraTest01.java
@@ -0,0 +1,11 @@
+package javacore.metodos.test;
+
+import javacore.metodos.dominio.Calculadora;
+
+public class CalculadoraTest01 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        calculadora.soma();
+        calculadora.subtracao();
+    }
+}
