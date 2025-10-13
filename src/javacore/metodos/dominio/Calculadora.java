218dc6d87bb6c439eaa3de43861124260adf5b7a
m9ts <mat3us.gois@gmail.com>
2025-10-13 15:10:40 -0300

    Método com passagem de parâmetros

--- src/javacore/metodos/dominio/Calculadora.java
+++ src/javacore/metodos/dominio/Calculadora.java
@@ -2,11 +2,15 @@ package javacore.metodos.dominio;
 
 public class Calculadora {
 
-    public void soma() {
+    public void somaDoisNumeros() {
         System.out.println(10 + 10);
     }
 
-    public void subtracao() {
+    public void subtraiDoisNumeros() {
         System.out.println(21 - 10);
+    }
+
+    public void multiplicaDoisNumeros(int num, float num2) {
+        System.out.println(num * num2);
     }
 }
---
+++ src/javacore/metodos/test/CalculadoraTest02.java
@@ -0,0 +1,10 @@
+package javacore.metodos.test;
+
+import javacore.metodos.dominio.Calculadora;
+
+public class CalculadoraTest02 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        calculadora.multiplicaDoisNumeros(5, 2.5F);
+    }
+}
