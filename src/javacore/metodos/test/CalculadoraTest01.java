311f55f6350e359ebbc8abc9732b328b8d6471ae
m9ts <mat3us.gois@gmail.com>
2025-10-13 15:11:05 -0300

    Ajuste no nome dos métodos de adição e subtração

--- src/javacore/metodos/test/CalculadoraTest01.java
+++ src/javacore/metodos/test/CalculadoraTest01.java
@@ -5,7 +5,7 @@ import javacore.metodos.dominio.Calculadora;
 public class CalculadoraTest01 {
     public static void main(String[] args) {
         Calculadora calculadora = new Calculadora();
-        calculadora.soma();
-        calculadora.subtracao();
+        calculadora.somaDoisNumeros();
+        calculadora.subtraiDoisNumeros();
     }
 }
