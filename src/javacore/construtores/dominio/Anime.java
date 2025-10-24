f6dcc313e5039420e7214b898f4e96afe9b49e19
m9ts <mat3us.gois@gmail.com>
2025-10-24 13:39:53 -0300

    Sobrecarga

--- src/javacore/construtores/dominio/Anime.java
+++ src/javacore/construtores/dominio/Anime.java
@@ -5,17 +5,24 @@ public class Anime {
     private String tipo;
     private int episodios;
     private String genero;
+    private String estudio;
 
     // Construtor da classe Anime
     public Anime(String nome, String tipo, int episodios, String genero) {
+        this(); // faz referência ao construtor Anime()
         this.nome = nome;
         this.tipo = tipo;
         this.episodios = episodios;
         this.genero = genero;
     }
 
-    public Anime(){
+    public Anime(String nome, String tipo, int episodios, String genero, String estudio){
+        this(nome, tipo, episodios, genero); // precisa ser a primeira linha do métodozx
+        this.estudio = estudio;
+    }
 
+    public Anime() {
+        System.out.println("Dentro do construtor sem argumentos.");
     }
 
 
@@ -24,6 +31,7 @@ public class Anime {
         System.out.println("Tipo: " + this.tipo);
         System.out.println("Episódios: " + this.episodios);
         System.out.println("Gênero: " + this.genero);
+        System.out.println("Estúdio: " + this.estudio);
     }
 
     public String getGenero() {
--- src/javacore/construtores/test/AnimeTest01.java
+++ src/javacore/construtores/test/AnimeTest01.java
@@ -4,12 +4,9 @@ import javacore.construtores.dominio.Anime;
 
 public class AnimeTest01 {
     public static void main(String[] args) {
-        Anime anime = new Anime("Haikyuu", "TV", 12, "Esporte");
-        Anime anime2 = new Anime();
+        Anime anime = new Anime("Haikyuu", "TV", 12, "Esporte", "Production IG");
 
 //        anime.init("Akudama Drive", "TV", 12, "Ação");
         anime.imprime();
-        anime2.imprime();
-
     }
 }
