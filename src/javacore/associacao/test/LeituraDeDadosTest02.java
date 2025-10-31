546619a10ce79380cf3d611acd0f5f8e63c817f7
m9ts <mat3us.gois@gmail.com>
2025-10-31 10:32:07 -0300

    Exercício sobre leitura de dados usando Scanner

---
+++ src/javacore/associacao/test/LeituraDeDadosTest02.java
@@ -0,0 +1,19 @@
+package javacore.associacao.test;
+
+import java.util.Scanner;
+
+public class LeituraDeDadosTest02 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Previsão do futuro");
+        System.out.println("Digite sua pergunta abaixo e eu responderei 'sim' ou 'não'");
+
+        String pergunta = input.nextLine();
+        if (pergunta.charAt(0) == ' ') {
+            System.out.println("SIM");
+        } else {
+            System.out.println("NÃO");
+        }
+    }
+}
