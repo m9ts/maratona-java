151ec1efaccf81a9fbdb63a689ceca84f591d78e
m9ts <mat3us.gois@gmail.com>
2025-10-31 10:26:03 -0300

    Leitura de dados com a classe Scanner

---
+++ src/javacore/associacao/test/LeituraDeDados.java
@@ -0,0 +1,23 @@
+package javacore.associacao.test;
+
+import java.util.Scanner;
+
+public class LeituraDeDados {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Insira seu nome: ");
+        String nome = entrada.nextLine();
+
+        System.out.println("Insira sua idade: ");
+        int idade = entrada.nextInt();
+
+        System.out.println("Insira M/F para seu sexo: ");
+        char sexo = entrada.next().charAt(0); // pega somente a primeira letra de um array de caracteres
+
+        System.out.println("------------------------");
+
+        System.out.println("Nome: " + nome + "\nIdade: " + idade + "\nSexo: " + sexo);
+
+    }
+}
