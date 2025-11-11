7a8ce0ce1cf8180b64cb7711c493645cd0c613b9
m9ts <mat3us.gois@gmail.com>
2025-11-11 10:45:03 -0300

    Enumeração - pt. 02 - Construtores e atributos

--- src/javacore/enumeracao/dominio/TipoCliente.java
+++ src/javacore/enumeracao/dominio/TipoCliente.java
@@ -4,6 +4,22 @@ package javacore.enumeracao.dominio;
 // Cada valor do enum é uma instância única e imutável, usada para representar estados, categorias ou opções predefinidas.
 // Resumo: use enum quando as opções forem limitadas, conhecidas e imutáveis.
 public enum TipoCliente {
-    PESSOA_FISICA,
-    PESSOA_JURIDICA
+    PESSOA_FISICA(1, "Pessoa Física"),
+    PESSOA_JURIDICA(2, "Pessoa Jurídica");
+
+    private int valor;
+    private String nomeRelatorio;
+
+    TipoCliente(int valor, String nomeRelatorio) {
+        this.valor = valor;
+        this.nomeRelatorio = nomeRelatorio;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public String getNomeRelatorio() {
+        return nomeRelatorio;
+    }
 }
--- src/javacore/enumeracao/test/ClienteTest01.java
+++ src/javacore/enumeracao/test/ClienteTest01.java
@@ -5,14 +5,10 @@ import javacore.enumeracao.dominio.TipoCliente;
 
 public class ClienteTest01 {
     public static void main(String[] args) {
-        Cliente cliente1 = new Cliente("Mateus", TipoCliente.PESSOA_FISICA);
-        Cliente cliente2 = new Cliente("Adagoberto", TipoCliente.PESSOA_JURIDICA);
-        Cliente cliente3 = new Cliente("Ana", TipoCliente.PESSOA_FISICA);
-        Cliente cliente4 = new Cliente("Maria", TipoCliente.PESSOA_JURIDICA);
+        Cliente cliente1 = new Cliente("Mateus", TipoCliente.PESSOA_FISICA, Cliente.TipoPagamento.CREDITO);
+        Cliente cliente2 = new Cliente("Adagoberto", TipoCliente.PESSOA_JURIDICA, Cliente.TipoPagamento.DEBITO);
 
         System.out.println(cliente1);
         System.out.println(cliente2);
-        System.out.println(cliente3);
-        System.out.println(cliente4);
     }
 }
