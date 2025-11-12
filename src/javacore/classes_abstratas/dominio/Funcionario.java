a196b8b8826044a3e49f17f8622171e25e166bd5
m9ts <mat3us.gois@gmail.com>
2025-11-12 12:50:56 -0300

    Classes abstratas - pt. 03 - Regras de métodos abstratos

--- src/javacore/classes_abstratas/dominio/Funcionario.java
+++ src/javacore/classes_abstratas/dominio/Funcionario.java
@@ -2,7 +2,7 @@ package javacore.classes_abstratas.dominio;
 
 // A classe abstrata serve como modelo/template para outras, e não pode ser instanciada diretamente
 // Não se pode criar um objeto de Funcionario de forma direta, por exemplo
-public abstract class Funcionario {
+public abstract class Funcionario extends Pessoa {
     protected String nome;
     protected double salario;
 
@@ -15,4 +15,9 @@ public abstract class Funcionario {
     // Métodos abstratos só existem em classes abstratas
     // Classes abstratas podem ter métodos concretos e métodos abstratos
     public abstract void calculaBonus();
+
+    @Override
+    public void imprime() {
+        System.out.println("Imprimindo...");
+    }
 }
---
+++ src/javacore/classes_abstratas/dominio/Pessoa.java
@@ -0,0 +1,7 @@
+package javacore.classes_abstratas.dominio;
+
+public abstract class Pessoa {
+    // A classe Funcionario estende da classe Pessoa, então as subclasses de
+    // Funcionario conseguirão implementar o método imprime() de Pessoa
+    public abstract void imprime();
+}
