52318a571cbb1a7983ee44539742b7ad16672991
m9ts <mat3us.gois@gmail.com>
2025-11-21 10:14:11 -0300

    Exceções - pt. 05 - Lançando exceção checked

---
+++ src/javacore/exception/exception/test/ExceptionTest02.java
@@ -0,0 +1,21 @@
+package javacore.exception.exception.test;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ExceptionTest02 {
+    public static void main(String[] args) throws IOException {
+        criarNovoArquivo();
+    }
+
+    private static void criarNovoArquivo() throws IOException {
+        File file = new File("arquivo\\test.txt");
+        try {
+            boolean isCriado = file.createNewFile();
+            System.out.println("Arquivo criado " + isCriado);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw e;
+        }
+    }
+}
