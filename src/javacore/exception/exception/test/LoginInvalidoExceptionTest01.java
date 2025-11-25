d6b4c4144ed2bd6064ef4f987804531113d63c3f
m9ts <mat3us.gois@gmail.com>
2025-11-25 12:38:29 -0300

    Exceções - pt. 10 - Exceção customizada

---
+++ src/javacore/exception/exception/test/LoginInvalidoExceptionTest01.java
@@ -0,0 +1,32 @@
+package javacore.exception.exception.test;
+
+import javacore.exception.exception.dominio.LoginInvalidoException;
+
+import java.util.Scanner;
+
+public class LoginInvalidoExceptionTest01 {
+    public static void main(String[] args) {
+        try {
+            logar();
+        } catch (LoginInvalidoException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void logar() throws LoginInvalidoException {
+        Scanner teclado = new Scanner(System.in);
+        String usernameDB = "Mxx";
+        String senhaDB = "SSJ";
+
+        System.out.println("Usuário: ");
+        String usernameDigitado = teclado.nextLine();
+
+        System.out.println("Senha: ");
+        String senhaDigitada = teclado.nextLine();
+
+        if (!usernameDB.equals(usernameDigitado) || !senhaDB.equals(senhaDigitada)) {
+            throw new LoginInvalidoException("Usuário ou senha inválidos!");
+        }
+        System.out.println("Usuário logado com sucesso.");
+    }
+}
