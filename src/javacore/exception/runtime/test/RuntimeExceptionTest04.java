8e44540ff3b8a876061374d8130924861ed44d5d
m9ts <mat3us.gois@gmail.com>
2025-11-25 10:54:26 -0300

    Exceções - pt. 08 - Multi catch em linha

--- src/javacore/exception/runtime/test/RuntimeExceptionTest04.java
+++ src/javacore/exception/runtime/test/RuntimeExceptionTest04.java
@@ -1,36 +1,35 @@
 package javacore.exception.runtime.test;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.SQLException;
 
 // Exceções genéricas devem sempre vir ao final do bloco de tratamento de erro
 // Ex.: RuntimeException
 
+// Multi catch em linha -> só pode ser usado com exceções que não estão dentro da mesma linha de herança
+// e são separados por pipe (|)
+
 public class RuntimeExceptionTest04 {
     public static void main(String[] args) {
         try {
             throw new RuntimeException();
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("Dentro do ArrayIndexOutOfBoundsException");
+        } catch (ArrayIndexOutOfBoundsException | IllegalArgumentException | ArithmeticException e) {
+            System.out.println("Dentro do ArrayIndexOutOfBoundsException | IllegalArgumentException | ArithmeticException");
         } catch (IndexOutOfBoundsException e) {
-            System.out.println("Dentro do ArrayIndexOutOfBoundsException");
-        } catch (IllegalArgumentException e) {
-            System.out.println("Dentro de IllegalArgumentException");
-        } catch (ArithmeticException e) {
-            System.out.println("Dentro de ArithmeticException");
+            System.out.println("Dentro do IndexOutOfBoundsException");
         } catch (RuntimeException e) {
             System.out.println("Dentro do RuntimeException");
         }
 
         try {
             talvezLanceException();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
+        } catch (IOException | SQLException e) {
+            e.printStackTrace();
         }
     }
 
     private static void talvezLanceException() throws SQLException, FileNotFoundException {
+
     }
 }
