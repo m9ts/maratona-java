d38b244ff1f9964f1778e8c9e49bb12714e514a3
m9ts <mat3us.gois@gmail.com>
2025-11-27 10:23:30 -0300

    Classes utilitárias - Wrappers - pt. 02

--- src/javacore/wrapper/test/WrapperTest01.java
+++ src/javacore/wrapper/test/WrapperTest01.java
@@ -23,5 +23,28 @@ public class WrapperTest01 {
         Character charW = 'W';
         Boolean booleanW = false;
 
+        // Autoboxing -> transformar tipo primitivo em wrapper
+        // Automático pelo Java
+        // Ex.: Integer intW = 1;
+
+        // Unboxing -> transformar wrapper em tipo primitivo
+        // Ex.: int i = intW;
+
+        // Integer.parseInt transforma String em número
+        Integer intW2 = Integer.parseInt("1");
+
+        boolean verdadeiro = Boolean.parseBoolean("true");
+        System.out.println(verdadeiro);
+
+        System.out.println("Character.isDigit: " + Character.isDigit('1'));
+        System.out.println("Character.isDigit: " + Character.isDigit('C'));
+        System.out.println("Character.isLetterOrDigit: " + Character.isLetterOrDigit('!'));
+        System.out.println("Character.isLowerCase: " + Character.isLowerCase('M'));
+        System.out.println("Character.toLowerCase: " + Character.toLowerCase('Z'));
+        System.out.println("Character.toUpperCase: " + Character.toUpperCase('x'));
+        System.out.println("Character.toTitleCase: " + Character.toTitleCase('y'));
+        System.out.println("Character.hashCode: " + Character.hashCode('G'));
+        System.out.println("Character.hashCode: " + Character.hashCode('g'));
+
     }
 }
