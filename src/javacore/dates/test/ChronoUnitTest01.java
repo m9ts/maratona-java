b381a965443011b502c82bb4629bc7f98fe7d775
m9ts <mat3us.gois@gmail.com>
2025-12-10 11:13:40 -0300

    Testando ChronoUnit

--- src/javacore/dates/test/ChronoUnitTest01.java
+++ src/javacore/dates/test/ChronoUnitTest01.java
@@ -2,6 +2,7 @@ package javacore.dates.test;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.Month;
 import java.time.temporal.ChronoUnit;
 
 public class ChronoUnitTest01 {
@@ -11,5 +12,26 @@ public class ChronoUnitTest01 {
         System.out.println("Diferença de semanas entre a semana do meu nascimento até a semana atual: " + ChronoUnit.WEEKS.between(aniversario, LocalDateTime.now()));
         System.out.println("Diferença de meses entre o mês do meu nascimento até o mês atual: " + ChronoUnit.MONTHS.between(aniversario, LocalDateTime.now()));
         System.out.println("Diferença de anos entre o ano do meu nascimento até o ano atual: " + ChronoUnit.YEARS.between(aniversario, LocalDateTime.now()));
+
+        LocalDate manuAniv = LocalDate.of(2005, 4, 19);
+        System.out.println("Diferença de dias entre o aniversário da Manu e o meu: " + ChronoUnit.DAYS.between(manuAniv, aniversario) + " dias");
+        System.out.println("Idade da Manu em semanas: " + ChronoUnit.WEEKS.between(manuAniv, LocalDate.now()) + " semanas");
+        System.out.println("Diferença de semanas entre o aniversário da Manu e o meu: " + ChronoUnit.WEEKS.between(manuAniv, aniversario) + " semanas");
+
+        LocalDate rafaAniv = LocalDate.of(2004, 02, 26);
+        System.out.println(ChronoUnit.DAYS.between(rafaAniv, aniversario));
+
+        LocalDate maeAniv = LocalDate.of(1976, 12, 5);
+        LocalDate paiAniv = LocalDate.of(1975, 10, 27);
+        LocalDate grandmaAniv = LocalDate.of(1950, Month.MARCH, 23);
+
+        System.out.println(ChronoUnit.DAYS.between(paiAniv, maeAniv));
+
+        System.out.println(ChronoUnit.DAYS.between(maeAniv, LocalDate.now()));
+        System.out.println(ChronoUnit.WEEKS.between(maeAniv, LocalDate.now()));
+        System.out.println(ChronoUnit.DAYS.between(paiAniv, LocalDate.now()));
+        System.out.println(ChronoUnit.WEEKS.between(paiAniv, LocalDate.now()));
+        System.out.println(ChronoUnit.DAYS.between(grandmaAniv, LocalDate.now()));
+
     }
 }
